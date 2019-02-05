b131895a8bc3335caefd7abd19f8ce6e044ab32d
Dawid Krakowczyk <35225469+DKrakowczyk@users.noreply.github.com>
2019-02-05 22:47:55 +0100

    initial commit

---
+++ game.js
@@ -0,0 +1,107 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+
+
+$(document).keypress(function(event) {
+  if (level == 0) {
+    nextSequence();
+  }
+});
+
+
+$(".btn").click(function() {
+  if (level > 0) {
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+    playSound(this.id);
+    animatePress(this.id);
+    checkAnser(userClickedPattern.length);
+  }
+});
+
+function nextSequence() {
+  var r = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[r];
+  gamePattern.push(randomChosenColour);
+  playSound(randomChosenColour);
+  animatePress(randomChosenColour);
+  console.log(randomChosenColour);
+  level++;
+  $("#level-title").text("Level " + level);
+}
+
+function playSound(button) {
+  var audio;
+  switch (button) {
+    case "red":
+      audio = new Audio("sounds/1.wav");
+      audio.play();
+      break;
+
+    case "green":
+      audio = new Audio("sounds/2.wav");
+      audio.play();
+      break;
+
+    case "yellow":
+      audio = new Audio("sounds/3.wav");
+      audio.play();
+      break;
+
+    case "blue":
+      audio = new Audio("sounds/4.wav");
+      audio.play();
+      break;
+
+    case "bad":
+      audio = new Audio("sounds/bad.wav");
+      audio.play();
+      break;
+      
+    default:
+      audio = new Audio("sounds/bad.wav");
+      audio.play();
+  }
+}
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColour).removeClass("pressed");
+  }, 200);
+}
+
+function checkAnser(currentLevel) {
+  var success;
+
+  for (var i = 0; i < currentLevel; i++) {
+    if (userClickedPattern[i] == gamePattern[i]) {
+      success = true;
+    } else {
+      success = false;
+
+    }
+  }
+  if (!success) {
+    playSound("bad");
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    level = 0;
+    gamePattern = [];
+    userClickedPattern = [];
+  }
+  if (currentLevel == gamePattern.length) {
+
+    if (success) {
+      setTimeout(nextSequence, 500);
+
+      userClickedPattern = [];
+    }
+
+  }
+}
